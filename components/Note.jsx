49e77591340878b609a8491f092fd0c93a5efe17
Kunwar <kunwarabhay05@gmail.com>
2024-08-21 22:02:41 +0530

    project done

--- components/Note.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function Note(){
-    return(<div className="note">
-        <h1>Title</h1>
-        <p>Content Lorem ipsum dolor sit amet.</p></div>);
-}
-export default Note;
--- components/footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const time= new Date().getFullYear();
-function Footer(){
-    return (<footer>
-        <p>Copyright {time}</p>
-        </footer>);
-}
-export default Footer;
